daffe86ff0fb5c2b7444e2289e6902c3bb2e857c
Muhammad Ali <alibinimran@gmail.com>
2019-05-07 11:48:43 +0500

    initial

---
+++ Project_VP_Summer/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Project_VP_Summer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dealer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.ForestGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(168, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(95, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(199, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(82, 211);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(171, 211);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(260, 211);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(349, 211);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(438, 211);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(83, 94);
+            this.pictureBox9.TabIndex = 10;
+            this.pictureBox9.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.button1.Location = new System.Drawing.Point(550, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.groupBox1.Location = new System.Drawing.Point(550, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 181);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Player Actions";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Draw card";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Call";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Count";
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.button4.Location = new System.Drawing.Point(7, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Project_VP_Summer/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_VP_Summer
+{
+    public partial class Form1 : Form
+    {
+        int dcard1, dcard2 = 0;
+        int dcard3 = 0;
+        int dscore = 0;
+        int drawcount = 0;
+        int pcard1, pcard2, pcard3, pcard4,pcard5= 0;
+        int psum = 0;
+        string[] deck = new string[] {"2_of_hearts", "3_of_hearts", "4_of_hearts", "5_of_hearts", "6_of_hearts", "7_of_hearts", "8_of_hearts", "9_of_hearts", "10_of_hearts",
+"2_of_clubs", "3_of_clubs", "4_of_clubs", "5_of_clubs", "6_of_clubs", "7_of_clubs", "8_of_clubs", "9_of_clubs", "10_of_clubs",
+"2_of_diamonds", "3_of_diamonds", "4_of_diamonds", "5_of_diamonds", "6_of_diamonds", "7_of_diamonds", "8_of_diamonds", "9_of_diamonds", "10_of_diamonds",
+"2_of_spades", "3_of_spades", "4_of_spades", "5_of_spades", "6_of_spades", "7_of_spades", "8_of_spades", "9_of_spades", "10_of_spades",
+"ace_of_clubs", "ace_of_hearts", "ace_of_diamonds", "ace_of_spades", "king_of_clubs", "king_of_hearts", "king_of_diamonds", "king_of_spades", "queen_of_clubs", "queen_of_spades", "queen_of_diamonds", "queen_of_hearts", "jack_of_clubs", "jack_of_spades", "jack_of_hearts", "jack_of_diamonds"};
+
+        int[] decknum = new int[]{2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,
+2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,11,11,11,11,10,10,10,10,10,10,10,10,10,10,10,10};
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(dscore < 21 && dscore>psum)
+            {
+                MessageBox.Show("Dealer Wins");
+            }
+            if (dscore == 21)
+            {
+                MessageBox.Show("Dealer Wins");
+            }
+            if (dscore > 21)
+            {
+                MessageBox.Show("Player Wins");
+            }
+            if (psum == 21)
+            {
+                MessageBox.Show("Player Wins");
+            }
+            if (psum < 21 && psum> dscore)
+            {
+                MessageBox.Show("Player Wins");
+            }
+            if (psum == dscore)
+            {
+                MessageBox.Show("TIE");
+            }
+            if (psum == 21 && dscore == 21)
+            {
+                MessageBox.Show("TIE");
+            }
+            if (psum > 21 && dscore > 21)
+            {
+                MessageBox.Show("TIE");
+            }
+            if(dscore > 21 && psum < 21)
+            {
+                MessageBox.Show("Player Wins");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dcard1 = psum = dcard2 = dcard3 = dscore= drawcount = pcard1 = pcard2 = pcard3 = pcard4 = pcard5 = 0;
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            pictureBox8.Image = null;
+            pictureBox9.Image = null;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// INITIALIZATION
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            dcard1 = decknum[rnd.Next(51)];
+            pictureBox1.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\back.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\back.png");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            dcard2 = decknum[rnd.Next(51)];
+            dscore = dcard1 + dcard2;
+            if(dscore < 7)
+            {
+                dcard3 = decknum[rnd.Next(51)];
+                dscore = dscore + dcard3;
+            }
+            int temp = rnd.Next(51);
+            int temp2 = rnd.Next(51);
+            pcard1 = decknum[temp];
+            pcard2 = decknum[temp2];
+            psum = pcard1 + pcard2;
+            pictureBox5.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\" + deck[temp] +".png");
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\" + deck[temp2] + ".png");
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            label3.Text = Convert.ToString(psum);
+        }
+
+        /////Draw Card
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            drawcount++;
+            Random rnd = new Random();
+            if (drawcount == 1)
+            {
+                int temp = rnd.Next(51);
+                pcard3 = decknum[temp];
+                psum = psum + pcard3;
+                label3.Text = Convert.ToString(psum);
+                pictureBox7.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\" + deck[temp] + ".png");
+                pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (drawcount == 2)
+            {
+                int temp = rnd.Next(51);
+                pcard4 = decknum[temp];
+                psum = psum + pcard4;
+                label3.Text = Convert.ToString(psum);
+                pictureBox8.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\" + deck[temp] + ".png");
+                pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (drawcount == 3)
+            {
+                int temp = rnd.Next(51);
+                pcard5 = decknum[temp];
+                psum = psum + pcard5;
+                label3.Text = Convert.ToString(psum);
+                pictureBox9.Image = Image.FromFile(@"C:\Users\Muhammad Ali\source\repos\Project_VP_Summer\Project_VP_Summer\png\" + deck[temp] + ".png");
+                pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Count Exceeded");
+            }
+        }
+    }
+}
